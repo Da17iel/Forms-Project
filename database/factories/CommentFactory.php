f382ff0151a60b5f7f0fee74f89a0a24fd5da045
Daniel Greil <danimickigreil@gmail.com>
2022-03-16 15:11:22 +0100

    Setup Database Seeders and Factories
    Created Models for User, Post and Comment. Also created DB Migrations, Seeders and Factories for them

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        // Decide if the Comment is a comment on a Post or another Comment
+        $postId = NULL;
+        $commentId = NULL;
+        if (rand(0, 1)) {
+            $postId = Post::all()->random();
+        } else {
+            $commentId = rand(1, 500);
+        }
+        return [
+            "user_id" => User::all()->random(),
+            "post_id" => $postId,
+            "comment_id" => $commentId,
+            "content" => $this->faker->sentence(12),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Post::factory(100)->create();
+        Comment::factory(500)->create();
     }
 }
